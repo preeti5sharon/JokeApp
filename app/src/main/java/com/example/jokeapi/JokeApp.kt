57fa0c8340cd47fbc55bb7e9eb7329a43b9ba8e8
preeti5sharon <85344743+preeti5sharon@users.noreply.github.com>
2023-03-22 17:55:30 +0530

    added firebase performance and crashlytics

--- app/src/main/java/com/example/jokeapi/JokeApp.kt
+++ app/src/main/java/com/example/jokeapi/JokeApp.kt
@@ -1,8 +1,13 @@
 package com.example.jokeapi
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class JokeApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+    }
 }
